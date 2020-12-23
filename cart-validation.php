ceadf307c7306eeef7f1f45ef2f7257f1c0e60be
Hemant <hemant.khutti@briteskies.com>
2020-12-23 11:42:27 +0530

    Added another function

--- cart-validation.php
+++ cart-validation.php
@@ -18,3 +18,23 @@ function logicrays_add_to_cart_validation( $passed, $product_id, $quantity ) {
     }
     return $passed;
 }
+
+// WooCommerce: Check if Product ID is in the Cart
+
+add_action( 'woocommerce_before_cart', 'logicrays_find_product_in_cart' );
+    
+function logicrays_find_product_in_cart() {
+  
+   $product_id = 282;
+  
+   $product_cart_id = WC()->cart->generate_cart_id( $product_id );
+   $in_cart = WC()->cart->find_product_in_cart( $product_cart_id );
+  
+   if ( $in_cart ) {
+  
+      $notice = 'Product ID ' . $product_id . ' is in the Cart!';
+      wc_print_notice( $notice, 'notice' );
+  
+   }
+  
+}
